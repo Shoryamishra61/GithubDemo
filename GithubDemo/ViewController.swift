89c4f17e590c48d2c154db22c3cea0fed0e56813
Shoryakumar Mishra <sm7078@srmist.edu.in>
2025-10-31 09:00:06 +0530

    Conflict to push to develop branch

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     print("develop branch changes")
+    print("CHanges in develop branch to see how conflict is solved")
     }
 
 
